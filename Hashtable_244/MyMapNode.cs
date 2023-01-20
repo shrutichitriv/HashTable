0a00b0ec3ca00ace642c3960fe477148769c6844
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-20 21:32:19 +0530

    [Shruti] Ability to find frequency of words in a sentence

---
+++ Hashtable_244/MyMapNode.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable_244
+{
+    public class MyMapNode<K, V>
+    {
+
+
+        public readonly int size;
+        public readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V> { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public void FreqOfWord(string[] arr)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var item in arr)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict[item] = 1;
+                }
+            }
+            foreach (var i in dict)
+            {
+                Console.WriteLine(i.Key + "=" + i.Value + "times");
+            }
+        }
+    }
+
+
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
--- Hashtable_244/Program.cs
+++ Hashtable_244/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using Hashtable_244;
+using System.Security.Cryptography;
 
 namespace HashTable
 {
@@ -7,6 +8,50 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Creating a project for Hash Table Problem");
+
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            string xyz = "To be or not to be";
+            string[] arr1 = xyz.Split(" ");
+            int kays = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                hash.Add(kays, arr1[i]);
+                kays++;
+            }
+
+            string hash0 = hash.Get(0);
+            Console.WriteLine("0th index value: " + hash0);
+
+            string hash1 = hash.Get(1);
+            Console.WriteLine("1st index value: " + hash1);
+
+            string hash2 = hash.Get(2);
+            Console.WriteLine("2nd index value: " + hash2);
+
+            string hash3 = hash.Get(3);
+            Console.WriteLine("3rd index value: " + hash3);
+
+            string hash4 = hash.Get(4);
+            Console.WriteLine("4th index value: " + hash4);
+
+            string hash5 = hash.Get(5);
+            Console.WriteLine("5th index value: " + hash5);
+
+            Console.WriteLine("\nFrequency of Words ==> ");
+
+            string[] arr = new string[hash.size];
+            for (int i = 0; i < hash.size; i++)
+            {
+                arr[i] = hash.Get(i);
+                //arr[i] = arr[i].ToLower();
+            }
+
+
+            hash.FreqOfWord(arr);
+
+
+            Console.ReadLine();
+
         }
     }
 }
