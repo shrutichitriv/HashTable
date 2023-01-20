cc8c87ed2a4d3e38d3f895e2904a90eca29fb21c
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-20 21:43:27 +0530

    [Shruti] Ability to find frequency of words in a large paragraph phrase

--- Hashtable_244/Program.cs
+++ Hashtable_244/Program.cs
@@ -9,33 +9,17 @@ namespace HashTable
         {
             Console.WriteLine("Creating a project for Hash Table Problem");
 
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            string xyz = "To be or not to be";
-            string[] arr1 = xyz.Split(" ");
+            Console.WriteLine("Please the Phrase");
+            string phrase = Console.ReadLine();
+            string[] inputPhraseArr = phrase.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(inputPhraseArr.Length);
+
             int kays = 0;
-            for (int i = 0; i < arr1.Length; i++)
+            for (int i = 0; i < inputPhraseArr.Length; i++)
             {
-                hash.Add(kays, arr1[i]);
+                hash.Add(kays, inputPhraseArr[i]);
                 kays++;
             }
-
-            string hash0 = hash.Get(0);
-            Console.WriteLine("0th index value: " + hash0);
-
-            string hash1 = hash.Get(1);
-            Console.WriteLine("1st index value: " + hash1);
-
-            string hash2 = hash.Get(2);
-            Console.WriteLine("2nd index value: " + hash2);
-
-            string hash3 = hash.Get(3);
-            Console.WriteLine("3rd index value: " + hash3);
-
-            string hash4 = hash.Get(4);
-            Console.WriteLine("4th index value: " + hash4);
-
-            string hash5 = hash.Get(5);
-            Console.WriteLine("5th index value: " + hash5);
 
             Console.WriteLine("\nFrequency of Words ==> ");
 
@@ -46,13 +30,9 @@ namespace HashTable
                 //arr[i] = arr[i].ToLower();
             }
 
-
             hash.FreqOfWord(arr);
-
-
-            Console.ReadLine();
-
         }
+    
     }
 }
 
